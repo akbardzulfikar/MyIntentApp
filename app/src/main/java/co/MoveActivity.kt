c791848184d494ddb7dab5b44003b72e4c432ac9
laksamana.akbar <laksamana.guntur@mekar.id>
2020-09-14 13:51:20 +0700

    Initial commit

---
+++ app/src/main/java/co/MoveActivity.kt
@@ -0,0 +1,12 @@
+package co
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.id.myintentapp.R
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
---
+++ app/src/main/java/co/id/myintentapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package co.id.myintentapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import co.MoveActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
+    }
+}
